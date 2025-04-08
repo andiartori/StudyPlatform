987194fde351861dc432387ee5bdbdc6a2d5422e
andiartori <rizkyandiarto@gmail.com>
2025-04-08 09:56:58 +0700

    update videos

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,7 +7,6 @@ import Navbar from "../components/Navbar";
 export default function Dashboard() {
 	return (
 		<div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-			<Navbar />
 			<div className="px-4 py-6 sm:px-0">
 				<h1 className="text-3xl font-bold text-gray-900">Study Modules</h1>
 				<div className="mt-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
